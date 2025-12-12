b3e0764a035422de69375f6236ad6de3e46cff94
xxRevenantx <beto_war_157@hotmail.com>
2025-12-12 15:14:37 -0600

    feat: :zap: Constancia termino

--- resources/views/livewire/admin/licenciaturas/submodulo/pdf/constanciaTerminoPDF.blade.php
+++ resources/views/livewire/admin/licenciaturas/submodulo/pdf/constanciaTerminoPDF.blade.php
@@ -1,40 +1,44 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>CONSTANCIA DE TÉRMINO | {{$alumno->nombre}} {{ $alumno->apellido_paterno }} {{ $alumno->apellido_materno }} </title>
+    <title>CONSTANCIA DE TÉRMINO | {{ $alumno->nombre }} {{ $alumno->apellido_paterno }} {{ $alumno->apellido_materno }}
+    </title>
 </head>
 <style>
-
-      @page { margin:0px 0px 0px 0px; }
-
-
-      .page-break {
-     page-break-after: always;
+    @page {
+        margin: 0px 0px 0px 0px;
     }
 
-        @font-face {
-            font-family: 'greatVibes';
-            font-style: normal;
-            src: url('{{ storage_path('fonts/GreatVibes-Regular.ttf') }}') format('truetype');
 
-        }
+    .page-break {
+        page-break-after: always;
+    }
 
-         @font-face {
+    @font-face {
+        font-family: 'greatVibes';
+        font-style: normal;
+        src: url('{{ storage_path('fonts/GreatVibes-Regular.ttf') }}') format('truetype');
+
+    }
+
+    @font-face {
         font-family: 'calibri';
         font-style: normal;
         src: url('{{ storage_path('fonts/calibri/calibri.ttf') }}') format('truetype');
 
     }
 
-       @font-face {
-    font-family: 'calibri';
-    font-style: bold;
-    font-weight: 700;
-    src: url('{{ storage_path('fonts/calibri/calibri-bold.ttf') }}') format('truetype');
+    @font-face {
+        font-family: 'calibri';
+        font-style: bold;
+        font-weight: 700;
+        src: url('{{ storage_path('fonts/calibri/calibri-bold.ttf') }}') format('truetype');
     }
 
 
@@ -46,13 +50,15 @@
         height: 100%;
         z-index: -1;
     }
-    .contenedor{
-        padding: 70px 80px; ;
-        margin-top:100px;
+
+    .contenedor {
+        padding: 70px 80px;
+        ;
+        margin-top: 100px;
         /* background: #d7d7d7; */
     }
 
-    .asunto{
+    .asunto {
         font-size: 20px;
 
         text-align: right;
@@ -69,15 +75,15 @@
 
     }
 
-    .lugar{
-         font-size: 20px;
-          text-align: right;
+    .lugar {
+        font-size: 20px;
+        text-align: right;
         color: #000;
         font-family: 'calibri';
         margin-top: -20px;
     }
 
-    .corresponda{
+    .corresponda {
         font-size: 25px;
         color: #000;
         font-family: 'calibri';
@@ -87,7 +93,7 @@
         text-transform: uppercase;
     }
 
-    .alumno{
+    .alumno {
         font-size: 50px;
         text-align: center;
         color: #000;
@@ -97,7 +103,7 @@
 
     }
 
-    .descripcion{
+    .descripcion {
         font-size: 20px;
         text-align: justify;
         color: #000;
@@ -106,7 +112,7 @@
         line-height: 20px;
     }
 
-    .descripcion2{
+    .descripcion2 {
         font-size: 20px;
         text-align: justify;
         color: #000;
@@ -116,7 +122,7 @@
     }
 
 
-    .licenciatura{
+    .licenciatura {
         text-align: center;
         color: #000;
         font-family: 'calibri';
@@ -127,8 +133,8 @@
 
 
 
-    .atentamente{
-         font-size: 17px;
+    .atentamente {
+        font-size: 17px;
         text-align: center;
         color: #000;
         font-family: 'calibri';
@@ -144,93 +150,101 @@
 
     }
 
-    .rector{
+    .rector {
         text-transform: uppercase;
     }
 
-    .directora{
+    .directora {
         text-transform: uppercase;
     }
-
 </style>
+
 <body>
     @php
-     $nombreCompleto = "{$alumno->nombre} {$alumno->apellido_paterno} {$alumno->apellido_materno}";
-     $nombreFormateado = Str::title(Str::lower($nombreCompleto)); // Por si viene con mayúsculas raras
+        $nombreCompleto = "{$alumno->nombre} {$alumno->apellido_paterno} {$alumno->apellido_materno}";
+        $nombreFormateado = Str::title(Str::lower($nombreCompleto)); // Por si viene con mayúsculas raras
+
+        $nombreRector = "{$rector->nombre} {$rector->apellido_paterno} {$rector->apellido_materno}";
+        $nombreDirectora = "{$directora->nombre} {$directora->apellido_paterno} {$directora->apellido_materno}";
+
+        $licenciaturaMayuscula = Str::upper($alumno->licenciatura->nombre);
+    @endphp
 
 
-    $nombreRector = "{$rector->nombre} {$rector->apellido_paterno} {$rector->apellido_materno}";
-    $nombreDirectora = "{$directora->nombre} {$directora->apellido_paterno} {$directora->apellido_materno}";
+    <div class="fondo">
+        <img src="{{ public_path('storage/carta_pasante.jpg') }}" alt="fondo" style="width: 100%; height: 100%;">
+    </div>
 
-    $licenciaturaMayuscula = Str::upper($alumno->licenciatura->nombre);
-@endphp
-
-
-        <div class="fondo">
-            <img src="{{ public_path('storage/carta_pasante.jpg') }}" alt="fondo" style="width: 100%; height: 100%;">
-        </div>
-
-        <div class="contenedor">
-            <p class="asunto">Asunto: Constancia de término y acreditación.</p>
-            <p class="lugar">
+    <div class="contenedor">
+        <p class="asunto">Asunto: Constancia de término y acreditación.</p>
+        <p class="lugar">
             Cd. Altamirano, Gro., a {{ \Carbon\Carbon::parse($fecha)->translatedFormat('d \d\e F \d\e\l Y') }}.
-          </p>
+        </p>
 
-          <p class="corresponda">A QUIEN CORRESPONDA:</p>
+        <p class="corresponda">A QUIEN CORRESPONDA:</p>
 
-          <p style="text-align: center; font-size: 20px; color: #000; font-family: 'calibri';  margin-top: 20px;">
-            Por este conducto se hace constar que la alumna
-          </p>
+        <p style="text-align: center; font-size: 20px; color: #000; font-family: 'calibri';  margin-top: 20px;">
+            Por este conducto se hace constar que
+            @if ($alumno->sexo == 'M')
+                la alumna
+            @else
+                el alumno
+            @endif
+        </p>
 
-          <p class="alumno">{{ $nombreFormateado }}</p>
+        <p class="alumno">{{ $nombreFormateado }}</p>
 
-          <p class="descripcion">
-           Con CURP de registro <b>{{$alumno->CURP}}</b>, cursó y acreditó
-           satisfactoriamente las asignaturas correspondientes a la Licenciatura en
-        <b>{{$licenciaturaMayuscula}}</b>, finalizando su último cuatrimestre el 20 de agosto del año en curso,
-        en la generación <b>{{$alumno->generacion->generacion}}</b>, y actualmente su documentación oficial se
-        encuentra en trámite ante las autoridades educativas correspondientes en la
-        Ciudad de Chilpancingo, Gro.
-          </p>
+        <p class="descripcion">
+            Con CURP de registro <b>{{ $alumno->CURP }}</b>, cursó y acreditó
+            satisfactoriamente las asignaturas correspondientes a la Licenciatura en
+            <b>{{ $licenciaturaMayuscula }}</b>, finalizando su último cuatrimestre el 20 de agosto del año en curso,
+            en la generación <b>{{ $alumno->generacion->generacion }}</b>, y actualmente su documentación oficial se
+            encuentra en trámite ante las autoridades educativas correspondientes en la
+            Ciudad de Chilpancingo, Gro.
+        </p>
 
-          <p class="descripcion2">
+        <p class="descripcion2">
             Para los fines legales que el interesado(a) convengan, y conforme a derecho se
-                extiende la presente a los
-                {{ \NumberFormatter::create('es', \NumberFormatter::SPELLOUT)->format(\Carbon\Carbon::parse($fecha)->day) }}
-                días del mes de {{ \Carbon\Carbon::parse($fecha)->translatedFormat('F') }} del {{ \Carbon\Carbon::parse($fecha)->translatedFormat('Y') }}
-                en Cd.
-                Altamirano,Gro.
+            extiende la presente a los
+            {{ \NumberFormatter::create('es', \NumberFormatter::SPELLOUT)->format(\Carbon\Carbon::parse($fecha)->day) }}
+            días del mes de {{ \Carbon\Carbon::parse($fecha)->translatedFormat('F') }} del
+            {{ \Carbon\Carbon::parse($fecha)->translatedFormat('Y') }}
+            en Cd.
+            Altamirano,Gro.
 
-          </p>
-
-
-
-          <p class="atentamente">
-           <b> A T E N T A M E N T E</b> <br>
-        EDUCACIÓN INTEGRAL ¡ELIGE CUM!
-          </p>
-
-
-          <table>
-                <tr>
-                    <td style="text-align: center;">
-                        <span style="display: block; border-top: 1px solid #000; width: 300px; margin: 0 auto;"></span>
-                        <span class="rector" style="font-family: 'calibri'; font-size: 17px; bold;">{{ $rector->titulo }} {{ $nombreRector }}</span><br>
-                        <span style="font-family: 'calibri'; font-size: 17px;">{{ $rector->cargo }}</span>
-                    </td>
-                    <td style="text-align: center;">
-                        <span style="display: block; border-top: 1px solid #000; width: 300px; margin: 0 auto;"></span>
-                        <span class="directora" style="font-family: 'calibri'; font-size: 17px; bold;">M.S.P.{{ $nombreDirectora }}</span><br>
-                        <span style="font-family: 'calibri'; font-size: 17px;">{{ $directora->cargo }}</span>
-                    </td>
-                </tr>
-          </table>
+        </p>
 
 
 
+        <p class="atentamente">
+            <b> A T E N T A M E N T E</b> <br>
+            EDUCACIÓN INTEGRAL ¡ELIGE CUM!
+        </p>
 
-        </div>
+
+        <table>
+            <tr>
+                <td style="text-align: center;">
+                    <span style="display: block; border-top: 1px solid #000; width: 300px; margin: 0 auto;"></span>
+                    <span class="rector" style="font-family: 'calibri'; font-size: 17px; bold;">{{ $rector->titulo }}
+                        {{ $nombreRector }}</span><br>
+                    <span style="font-family: 'calibri'; font-size: 17px;">{{ $rector->cargo }}</span>
+                </td>
+                <td style="text-align: center;">
+                    <span style="display: block; border-top: 1px solid #000; width: 300px; margin: 0 auto;"></span>
+                    <span class="directora"
+                        style="font-family: 'calibri'; font-size: 17px; bold;">M.S.P.{{ $nombreDirectora }}</span><br>
+                    <span style="font-family: 'calibri'; font-size: 17px;">{{ $directora->cargo }}</span>
+                </td>
+            </tr>
+        </table>
+
+
+
+
+    </div>
 
 
 </body>
+
 </html>
--- resources/views/livewire/admin/licenciaturas/submodulo/pdf/credencialProfesorPDF.blade.php
+++ resources/views/livewire/admin/licenciaturas/submodulo/pdf/credencialProfesorPDF.blade.php
@@ -1,39 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CREDENCIAL DEL PROFESOR</title>
 
-     <style>
-         @page { margin:30px 0px 0px 0px; }
+    <style>
+        @page {
+            margin: 30px 0px 0px 0px;
+        }
 
         @font-face {
             font-family: 'calibri';
             font-style: normal;
             src: url('{{ storage_path('fonts/calibri/calibri.ttf') }}') format('truetype');
 
-         }
+        }
 
-       @font-face {
-        font-family: 'calibri';
-        font-style: bold;
-        font-weight: 700;
-        src: url('{{ storage_path('fonts/calibri/calibri-bold.ttf') }}') format('truetype');
-         }
+        @font-face {
+            font-family: 'calibri';
+            font-style: bold;
+            font-weight: 700;
+            src: url('{{ storage_path('fonts/calibri/calibri-bold.ttf') }}') format('truetype');
+        }
 
-         body{
+        body {
             font-family: 'calibri'
-         }
+        }
 
 
-         .contenedorCredenciales{
+        .contenedorCredenciales {
             width: 100%;
             /* background: #000; */
             margin: auto;
             padding: 0 0 0 70px;
-         }
+        }
+
         .credenciales {
             border: 1px solid #000;
             width: 18cm;
@@ -42,7 +46,8 @@
             /* padding: 10px; */
 
         }
-        .imagen{
+
+        .imagen {
             width: 50px;
             position: absolute;
             right: 90px;
@@ -50,8 +55,7 @@
 
         }
 
-        .titulo
-        {
+        .titulo {
             font-size: 10px;
             color: #fff;
             margin-top: -164px;
@@ -70,35 +74,34 @@
         .page-break {
             page-break-after: always;
         }
-
-
-
-
-     </style>
+    </style>
 </head>
-<body>
-      <div class="contenedorCredenciales">
 
-     @foreach ($profesores as $index => $profesor)
+<body>
+    <div class="contenedorCredenciales">
+
+        @foreach ($profesores as $index => $profesor)
             {{-- <img class="imagen" src="{{  public_path('storage/licenciaturas/'.$profesor->licenciatura->imagen) }}" alt=""> --}}
             <img class="credenciales" src="{{ public_path('storage/credencial-frontal-profesor.jpg') }}">
-        {{-- <div class="credenciales" style="background-image: url('{{ public_path('storage/credencial-frontal.png') }}')"> --}}
+            {{-- <div class="credenciales" style="background-image: url('{{ public_path('storage/credencial-frontal.png') }}')"> --}}
 
             <div class="info">
                 <h1 class="titulo">CREDENCIAL DEL PROFESOR</h1>
-                <b>Nombre:</b>{{ $profesor->nombre }} {{ $profesor->apellido_paterno }} {{ $profesor->apellido_materno }}<br>
+                <b>Nombre:</b>{{ $profesor->nombre }} {{ $profesor->apellido_paterno }}
+                {{ $profesor->apellido_materno }}<br>
                 <b>Título:</b> {{ $profesor->perfil ?? 'No especificado' }} <br>
                 <b>CURP:</b> {{ $profesor->user->CURP }} <br>
-                <b>Ciclo escolar:</b>  {{ $ciclo_escolar->ciclo_escolar }} <br>
-                <b>Vigencia:</b> Agosto {{substr($ciclo_escolar->ciclo_escolar, -4)}}
+                <b>Ciclo escolar:</b> {{ $ciclo_escolar->ciclo_escolar }} <br>
+                <b>Vigencia:</b> Agosto {{ substr($ciclo_escolar->ciclo_escolar, -4) }}
             </div>
 
 
-        @if (($index + 1) % 4 === 0)
-            <div class="page-break"></div>
-        @endif
-    @endforeach
-      </div>
+            @if (($index + 1) % 4 === 0)
+                <div class="page-break"></div>
+            @endif
+        @endforeach
+    </div>
 
 </body>
+
 </html>
